574b26c577c67b7bc17261924db77574d64a7ddf
Herenyon <rikitur17@gmail.com>
2023-02-20 15:28:25 +0100

    fine esercizio con commenti e un pizzico di css

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
+// costante per la richiesta del nome 
 const nome = prompt('inserisci il tuo nome');
+// costante per la richiesta del cognome 
 const cognome = prompt('inserisci il tuo cognome');
-const colorePreferito = prompt('inserisci il tuo colore preferito')
-
+// costante per la richiesta del colore preferito 
+const colorePreferito = prompt('inserisci il tuo colore preferito');
+// costante per unire insieme le costanti chiamate sopra 
+const password = nome + cognome + colorePreferito + 89;
+// stampa della costante password in console e nella pagina html 
+console.log(password);
+document.getElementById("demo").innerHTML = password;
